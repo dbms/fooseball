ff6f2e5115786f52673d67c5520a3f33b4a8afad
dbms <ideepakbalhara@gmail.com>
2019-12-01 21:05:12 +0530

    debug turned false

--- fooseballgame/settings.py
+++ fooseballgame/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '####################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['demo-fooseball.herokuapp.com', '127.0.0.1', 'localhost']
 
